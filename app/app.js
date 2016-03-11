a4392b6ea462f459ef03d2d7446662fdb30ea9c2
gfed53 <gfederico7153@gmail.com>
2016-03-11 13:03:09 -0500

    Directive working

--- app/app.js
+++ app/app.js
@@ -3,6 +3,25 @@ angular.module('myApp', [])
 .directive('makeEditable', function(){
 	return {
 		restrict: 'A',
-		templateUrl: 'editable.html'
+		templateUrl: 'editable.html',
+		scope: true,
+		transclude: true,
+		link: function(scope, element, attrs){
+			var text = element.find('p')[0],
+			button = element.find('input')[0];
+			scope.toggleEdit = function(){
+				if(text.getAttribute("contenteditable")==="false"){
+					text.setAttribute("contenteditable", "true");
+					text.style.color = "red";
+					button.setAttribute("value", "Save");
+				} else {
+					text.setAttribute("contenteditable", "false");
+					text.style.color = "black";
+					button.setAttribute("value", "Edit");
+				}
+				console.log(text);
+				// console.log(text.getAttribute("contenteditable"));
+			};
+		},
 	}
 })
